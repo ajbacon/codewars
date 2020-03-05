fb2485daa565a1e024aa0c080fb22e15f54b7969
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-05 17:12:37 +0000

    verify correct field. needs refactor

--- ruby/battleship_field/lib/battleship_field_validator.rb
+++ ruby/battleship_field/lib/battleship_field_validator.rb
@@ -1,32 +1,59 @@
 # frozen_string_literal: true
 
-def field_validator(input)
-  field = input
-  'Nope, something is wrong!'
+def validate_battlefield(field)
+  height = field.length
+  width = field[0].length
+  expected_ships = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
+  ships = []
+
+  (0...height).each do |i|
+    (0...width).each do |j|
+      next unless field[i][j] == 1
+
+      check_ship = valid_ship([i, j], field)
+      return false if check_ship == false
+
+      ships << check_ship[0]
+      vStart = i
+      hStart = j
+      (0...check_ship[0]).each do |_i|
+        field[vStart][hStart] = 2
+        check_ship[1] == 'v' ? vStart += 1 : hStart += 1
+      end
+    end
+  end
+
+  ships.sort == expected_ships
 end
 
 def valid_ship(start, field)
+  field_size = field.length - 1
   length = 0
   vStart = start[0]
   hStart = start[1]
 
-  direction = 'v' if field[vStart + 1][hStart] == 1
-  direction = 'h' if field[vStart][hStart + 1] == 1
+  if vStart <= field_size
+    direction = 'v' if field[vStart + 1][hStart] == 1
+  end
+  if hStart <= field_size
+    direction = 'h' if field[vStart][hStart + 1] == 1
+  end
 
   while field[vStart][hStart] == 1
     length += 1
     direction == 'v' ? vStart += 1 : hStart += 1
+    break if vStart > field_size || hStart > field_size
   end
 
   v1 = (start[0] - 1) < 0 ? 0 : (start[0] - 1)
   h1 = (start[1] - 1) < 0 ? 0 : (start[1] - 1)
 
   if direction == 'v'
-    v2 = start[0] + length
-    h2 = start[1] + 1
+    v2 = [start[0] + length, field_size].min
+    h2 = [start[1] + 1, field_size].min
   else
-    v2 = start[0] + 1
-    h2 = start[1] + length
+    v2 = [start[0] + 1, field_size].min
+    h2 = [start[1] + length, field_size].min
   end
 
   total = 0
@@ -36,6 +63,5 @@ def valid_ship(start, field)
     end
   end
 
-  return length == total ? length : nil
-  # p "length: #{length}, total: #{total}"
+  length == total ? [length, direction] : false
 end
--- ruby/battleship_field/spec/battleship_field_validator_spec.rb
+++ ruby/battleship_field/spec/battleship_field_validator_spec.rb
@@ -2,30 +2,36 @@ require 'battleship_field_validator'
 require 'field_examples'
 # frozen_string_literal: true
 
-describe '.field_validator' do
-  it 'should report an invalid field if no ships are input' do
+describe '.validate_battlefield' do
+  it 'should return false if an empty field' do
     field = field_example('empty_field')
-    expect(field_validator(field)).to eq('Nope, something is wrong!')
+    expect(validate_battlefield(field)).to eq(false)
+  end
+
+  it 'should return true for a valid field' do
+    field = field_example('valid_field')
+    expect(validate_battlefield(field)).to eq(true)
   end
 
   describe '.valid_ship' do
     it 'should return the ship length if it is valid and oriented horizontally' do
       field = field_example('valid_ship_4h')
-      expect(valid_ship([0, 0], field)).to eq(4)
+      expect(valid_ship([0, 0], field)).to eq([4, 'h'])
     end
 
     it 'should return the ship length if it is valid and oriented horizontally' do
       field = field_example('valid_ship_3v')
-      expect(valid_ship([0, 0], field)).to eq(3)
+      expect(valid_ship([0, 0], field)).to eq([3, 'v'])
     end
 
     it 'should return the ship length if it is valid and oriented horizontally' do
       field = field_example('valid_ship_2v')
-      expect(valid_ship([2, 4], field)).to eq(2)
+      expect(valid_ship([2, 4], field)).to eq([2, 'v'])
     end
+
     it 'should return nil if the ship is not valid' do
       field = field_example('invalid_ship_4h')
-      expect(valid_ship([1, 1], field)).to eq(nil)
+      expect(valid_ship([1, 1], field)).to eq(false)
     end
   end
 end
