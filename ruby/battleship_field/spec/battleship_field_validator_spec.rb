60acc8a921bb0f67e570f0fe413ac39354fa8f08
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-05 14:15:11 +0000

    refactor tests to clean up

--- ruby/battleship_field/spec/battleship_field_validator_spec.rb
+++ ruby/battleship_field/spec/battleship_field_validator_spec.rb
@@ -1,67 +1,32 @@
 require 'battleship_field_validator'
+require 'field_examples'
 # frozen_string_literal: true
 
 describe '.field_validator' do
-  empty_field = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-  valid_ship_4h = [[1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-  valid_ship_3v = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-  valid_ship_2v = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
   it 'should report an invalid field if no ships are input' do
-    expect(field_validator(empty_field)).to eq('Nope, something is wrong!')
+    field = field_example('empty_field')
+    expect(field_validator(field)).to eq('Nope, something is wrong!')
   end
 
   describe '.valid_ship' do
     it 'should return the ship length if it is valid and oriented horizontally' do
-      expect(valid_ship([0, 0], valid_ship_4h)).to eq(4)
+      field = field_example('valid_ship_4h')
+      expect(valid_ship([0, 0], field)).to eq(4)
     end
 
     it 'should return the ship length if it is valid and oriented horizontally' do
-      expect(valid_ship([0, 0], valid_ship_3v)).to eq(3)
+      field = field_example('valid_ship_3v')
+      expect(valid_ship([0, 0], field)).to eq(3)
     end
 
     it 'should return the ship length if it is valid and oriented horizontally' do
-      expect(valid_ship([1, 4], valid_ship_2v)).to eq(2)
+      field = field_example('valid_ship_2v')
+      expect(valid_ship([1, 4], field)).to eq(2)
     end
+
+    # it 'should return null if the ship is not valid' do
+    #   expect(valid_ship([1, 4], valid_ship_2v)).to eq(2)
+    # end
   end
 end
 
