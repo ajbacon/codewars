6d98e16db55ed2e0ea199f395016ba8b4517f2a1
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-05 12:31:43 +0000

    setup battleship validator

---
+++ ruby/battleship_field/lib/battleship_field_validator.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+def field_validator(input)
+  field = input
+  'Nope, something is wrong!'
+end
---
+++ ruby/battleship_field/spec/battleship_field_validator_spec.rb
@@ -0,0 +1,24 @@
+require 'battleship_field_validator'
+# frozen_string_literal: true
+
+describe '.field_validator' do
+  empty_field = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+  it 'should report an invalid field if no ships are input' do
+    expect(field_validator(empty_field)).to eq('Nope, something is wrong!')
+  end
+end
+
+# Todo
+
+# Function to check whether adjacent ships
+# Fuction to check whether the correct ships are present
