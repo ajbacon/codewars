8228066a6d234c0dc097dfcd6c44dd98450983f5
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-05 13:48:53 +0000

    calculate valid ship lengths

--- ruby/battleship_field/lib/battleship_field_validator.rb
+++ ruby/battleship_field/lib/battleship_field_validator.rb
@@ -4,3 +4,19 @@ def field_validator(input)
   field = input
   'Nope, something is wrong!'
 end
+
+def valid_ship(start, field)
+  length = 0
+  vStart = start[0]
+  hStart = start[1]
+
+  direction = 'v' if field[vStart + 1][hStart] == 1
+  direction = 'h' if field[vStart][hStart + 1] == 1
+
+  while field[vStart][hStart] == 1
+    length += 1
+    direction == 'v' ? vStart += 1 : hStart += 1
+  end
+
+  length
+end
--- ruby/battleship_field/spec/battleship_field_validator_spec.rb
+++ ruby/battleship_field/spec/battleship_field_validator_spec.rb
@@ -13,8 +13,55 @@ describe '.field_validator' do
                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
+  valid_ship_4h = [[1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+  valid_ship_3v = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+  valid_ship_2v = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
   it 'should report an invalid field if no ships are input' do
     expect(field_validator(empty_field)).to eq('Nope, something is wrong!')
+  end
+
+  describe '.valid_ship' do
+    it 'should return the ship length if it is valid and oriented horizontally' do
+      expect(valid_ship([0, 0], valid_ship_4h)).to eq(4)
+    end
+
+    it 'should return the ship length if it is valid and oriented horizontally' do
+      expect(valid_ship([0, 0], valid_ship_3v)).to eq(3)
+    end
+
+    it 'should return the ship length if it is valid and oriented horizontally' do
+      expect(valid_ship([1, 4], valid_ship_2v)).to eq(2)
+    end
   end
 end
 
